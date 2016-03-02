3170e1e9354b1c3763bc9f8b6ab85e45f8d1df18
John Domingo <johnwill.domingo@gmail.com>
2016-03-01 20:19:50 -0800

    Clean up white space and indentation

--- client/components/welcome/Welcome.js
+++ client/components/welcome/Welcome.js
@@ -2,16 +2,11 @@ import React from 'react'
 import {Jumbotron} from 'react-bootstrap'
 
 export default class Welcome extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-
-		return (
-				<Jumbotron className='jumbotron'>
-				<span className='welcomeText'> Welcome to Tourganic</span>
-				</Jumbotron>
-		)
-	}
+  render() {
+    return (
+      <Jumbotron className='jumbotron'>
+        <span className='welcomeText'>Welcome</span>
+      </Jumbotron>
+    )
+  }
 }
