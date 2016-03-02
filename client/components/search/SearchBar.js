62b7fc5beb5d799eb998480618e679ef429c01c1
John Domingo <johnwill.domingo@gmail.com>
2016-03-01 20:19:32 -0800

    Remove default option value to be react compliant
    Clean up tab space

--- client/components/search/SearchBar.js
+++ client/components/search/SearchBar.js
@@ -1,19 +1,18 @@
 import React from 'react'
 import {Button} from 'react-bootstrap'
 
-
 export default class SearchBar extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
+  constructor(props) {
+    super(props)
+    this.state = {
         name: "",
         city: "",
         price: ""
     }
-	}
+  }
 
   // Sets the state as user inputs search
-	handleInputChange(prop, e) {
+  handleInputChange(prop, e) {
     var newState = {}
     newState[prop] = e.target.value;
     this.setState(newState);
@@ -27,24 +26,24 @@ export default class SearchBar extends React.Component {
 }
 
 
-	render() {
-		return (
-			<div className="searchBarContainer">
-			  <form className="searchForm">
-		        <input className="searchInput" value={this.state.name} placeholder="Tour Name" onChange = {this.handleInputChange.bind(this, "name")} onClick={this.reset.bind(this, 'name')}/>
+  render() {
+    return (
+      <div className="searchBarContainer">
+        <form className="searchForm">
+            <input className="searchInput" value={this.state.name} placeholder="Tour Name" onChange = {this.handleInputChange.bind(this, "name")} onClick={this.reset.bind(this, 'name')}/>
             <input className="searchInput" value={this.state.city} placeholder="City" onChange = {this.handleInputChange.bind(this,"city")} onClick={this.reset.bind(this, 'city')}/>
             <select className="searchInput" value={this.state.price} onChange = {this.handleInputChange.bind(this,'price')}>
-    				  <option value="" selected>Price Range</option>
-    				  <option>$</option>
-    				  <option>$$</option>
-    				  <option>$$$</option>
-    				  <option>$$$$</option>
-				    </select>
-		        <Button bsStyle='default' bsSize='small' className="searchInput" onClick = { (e) => this.props.getToursFromDatabase(this.state) }>
+              <option selected>Price Range</option>
+              <option>$</option>
+              <option>$$</option>
+              <option>$$$</option>
+              <option>$$$$</option>
+            </select>
+            <Button bsStyle='default' bsSize='small' className="searchInput" onClick = { (e) => this.props.getToursFromDatabase(this.state) }>
               Search
             </Button>
-			  </form>
-			</div>
-		)
-	}
+        </form>
+      </div>
+    )
+  }
 };
