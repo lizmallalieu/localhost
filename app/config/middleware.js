6f98beb85b3e501929959e256a643015674c8675
John Domingo <johnwill.domingo@gmail.com>
2016-03-03 13:41:31 -0800

    Add webpack middleware to integrate with server

--- app/config/middleware.js
+++ app/config/middleware.js
@@ -9,6 +9,23 @@ module.exports = function(app, express) {
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
 
+  (function() {
+    var webpack = require('webpack');
+    var webpackConfig = require(process.env.WEBPACK_CONFIG ? process.env.WEBPACK_CONFIG : '../../config/webpack.config');
+    var compiler = webpack(webpackConfig);
+
+    app.use(require('webpack-dev-middleware')(compiler, {
+      inline: true,
+      hot: true,
+      noInfo: true,
+      publicPath: webpackConfig.output.publicPath
+    }));
+
+    app.use(require('webpack-hot-middleware')(compiler, {
+      log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
+    }));
+  })();
+
   app.use(express.static(path.resolve(__dirname, '../../public')));
 
   app.use(function (req, res, next) {
