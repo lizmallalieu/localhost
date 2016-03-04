40557e66bfce8ad03acd95773ba1fb84d000c29f
John Domingo <johnwill.domingo@gmail.com>
2016-03-04 10:32:48 -0800

    Integrate webpack middleware with server

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -2,28 +2,27 @@ var path = require('path');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 
+var webpack = require('webpack');
+var webpackConfig = require(process.env.WEBPACK_CONFIG ? process.env.WEBPACK_CONFIG : '../../config/webpack.config');
+var compiler = webpack(webpackConfig);
+
 module.exports = function(app, express) {
   app.use(morgan('dev'));
 
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
 
-  (function() {
-    var webpack = require('webpack');
-    var webpackConfig = require(process.env.WEBPACK_CONFIG ? process.env.WEBPACK_CONFIG : '../../config/webpack.config');
-    var compiler = webpack(webpackConfig);
+  app.use(require('webpack-dev-middleware')(compiler, {
+    inline: true,
+    hot: true,
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath
+  }));
 
-    app.use(require('webpack-dev-middleware')(compiler, {
-      inline: true,
-      hot: true,
-      noInfo: true,
-      publicPath: webpackConfig.output.publicPath
-    }));
-
-    app.use(require('webpack-hot-middleware')(compiler, {
-      log: console.log, path: '/__webpack_hmr', heartbeat: 10 * 1000
-    }));
-  })();
+  app.use(require('webpack-hot-middleware')(compiler, {
+    log: console.log,
+    path: '/__webpack_hmr', heartbeat: 10 * 1000
+  }));
 
   app.use(express.static(path.resolve(__dirname, '../../public')));
 
