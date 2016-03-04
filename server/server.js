fa74cb77bae1c94648e66aa55e6319c0a0da66a0
John Domingo <johnwill.domingo@gmail.com>
2016-03-04 10:45:21 -0800

    Remove unnecessary module imports

--- server/server.js
+++ server/server.js
@@ -1,9 +1,5 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-var mongoose = require("mongoose");
-var db = require('../config/db.js');
 
 //* ------------------------- */
 /*     MIDDLEWARE & ROUTES    */
