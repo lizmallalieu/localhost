49d44d2c2ebb2340d78a5a8bfac1b016f8c554c9
John Domingo <johnwill.domingo@gmail.com>
2016-03-04 11:14:46 -0800

    Import database config to connect to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
+require('../config/db');
 
 //* ------------------------- */
 /*     MIDDLEWARE & ROUTES    */
 /* -------------------------- */
 
+var app = express();
 require('./config/middleware')(app, express);
 require('./config/router')(app, express);
 
@@ -17,7 +19,7 @@ app.listen(port, 'localhost', function (err, result) {
   if (err) {
     console.log(err);
   }
-  console.log('Node Server + Webpack Hot Middleware is listening on', port);
+  console.log('🌏  Server listening on', port);
 });
 
 exports = module.exports = app;
