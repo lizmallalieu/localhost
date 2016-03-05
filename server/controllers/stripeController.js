02b407da67d55dc3502d173c5cee91a24a99d7da
Benny Hung <bennyhungry@gmail.com>
2016-03-05 01:33:18 -0800

    Add application fee to stripeController

--- server/controllers/stripeController.js
+++ server/controllers/stripeController.js
@@ -1,59 +1,26 @@
-var stripe = require("stripe")(
-  "sk_test_mZdHc2EKHCmqxjf94SlTaAOi"
-);
+var stripe = require('stripe')(process.env.API_STK_CLIENT);
 
-// retrieve current account balance
-stripe.balance.retrieve(function(err, balance) {
-  // asynchronously called
-});
+module.exports = {
+  chargeUser: function(req, res) {
+    // Get the credit card details submitted by the form
+    var token = req.body.stripeToken;
+    var amount = 1000; // in cents
 
-
-////////////
-///CHARGES//
-////////////
-
-// create charge
-stripe.charges.create({
-  amount: 400,
-  currency: "usd",
-  source: {
-    number: '4242424242424242',
-    exp_month: 12,
-    exp_year: 2017,
-    cvc: '123'
-  },
-  description: "Charge for test@example.com"
-}, function(err, charge) {
-  if (err) {
-
-  } else {
-    
+    stripe.charges.create({
+        card: token,
+        // source: token, don't know if card or source better
+        currency: 'usd',
+        amount: amount,
+        description: "Example charge",
+        application_fee: 123 // (we get) in cents
+    },
+    {stripe_account: CONNECTED_STRIPE_ACCOUNT_ID},
+    function(err, charge) {
+        if (err) {
+            res.send(500, err);
+        } else {
+            res.send(204);
+        }
+    });
   }
-});
-
-// returns list of your previous charges (most recent charges at the top)
-stripe.charges.list(
-  { limit: 3 },
-  function(err, charges) {
-    // asynchronously called
-  }
-);
-
-////////////
-//CUSTOMERS/
-////////////
-
-// create customer - autoassigns a customer id "id" as well used below in .retrieve();
-stripe.customers.create({
-  description: 'Customer for test@example.com',
-  source: "tok_17lMoGLQi5m0Mz6rPyMgtOfz" // obtained with Stripe.js
-}, function(err, customer) {
-  // asynchronously called
-});
-
-stripe.customers.retrieve(
-  "cus_81Qx1Cv8CZb3Ex",
-  function(err, customer) {
-    // asynchronously called
-  }
-);
+}
