9ddbc62558b133b8645129c6d7ba889d47858a48
Elizabeth Mallalieu <lizmallalieu@gmail.com>
2016-04-24 23:23:27 -0400

    Modify config

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.js$/,
+        test: /\.jsx$/,
         exclude: /node_modules/,
         loaders: ['react-hot', 'babel']
       },
